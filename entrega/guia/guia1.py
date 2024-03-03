17d255b24e70100027d37a9d616f750b15d8f0ae
sbockelmann <161514307+sbockelmann@users.noreply.github.com>
2024-03-03 22:45:44 +0000

    tarea

--- entrega/guia/guia1.py
+++ entrega/guia/guia1.py
@@ -1,16 +1,39 @@
+#punto 1.a
 print("Las máquinas me sorprenden con mucha frecuencia")
+
+#punto 1.b
 print("")
+
+#punto 1.c
 print("HOLA MUNDO")
+
+#punto 1.d
 print("23")
 print(23)
+
+#punto 1.e
 print("Una computadora puede ser llamada", '"inteligente"',"si logra engañar a una persona haciéndole creer que es un humano")
+
+#punto 1.f
 print("Susana Gabriela","Bockelmann Miller","36")
+
+#punto 1.g
 print("Susana Gabriela","Bockelmann Miller","36",sep="_")
+
+#punto 1.h
 print("calle","numero","codigopostal",sep="\t")
+
+#punto 1.i
 print("\n","calle","\n","numero","\n","codigopostal")
+
+#punto 1.j
 print("\n","feliz","\n","\t","Primavera","\n","\n","\t","\t","\t","2024")
+
+#punto 1.k
 print("Solo podemos ver poco del futuro", end=" ")
 print("pero lo suficiente para darnos cuenta de que hay mucho que hacer")
+
+#punto 1.l
 print("      *")
 print("    *   *")
 print("   *     *")
@@ -20,3 +43,33 @@ print("    *   *")
 print("    *   *")
 print("    *****")
 print()
+
+#punto 2
+print(5+7)
+print(0-9)
+print(2*45)
+print(3/2)
+print(6**2)
+print(3//2)
+print(2^2+ 6*2+ 9)
+
+#punto 3 y 4
+nombre='susana' #str
+apellido= 'bockelmann' #str
+edad= 36 #int
+altura =1,73 #floar
+numero_vuelo= 34532 #int
+temperatura= 21 #int
+salario_mensual=20321 #floar
+juego_terminado= ['fin','del','juego'] # str
+n=2
+if n % 2 == 0:
+    print(n, "es par.")
+
+#punto 5. Utilizando la función input
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
+edad = input("Ingrese su edad: ")
+print("¡Hola {nombre} {apellido} de {edad} años! Sé creativo.")
+
+
