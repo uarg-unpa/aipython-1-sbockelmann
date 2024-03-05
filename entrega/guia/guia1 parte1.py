a4515fcbdaab173ccff5d5fa6ecd9d8ab3ec3055
sbockelmann <161514307+sbockelmann@users.noreply.github.com>
2024-03-05 12:47:15 +0000

    tarea

--- entrega/guia/guia1 parte1.py
+++ entrega/guia/guia1 parte1.py
@@ -13,3 +13,15 @@ peso = float(input("ingrese su peso (en kg): "))
 altura = float(input("Ingrese su altura (en metro): "))
 imc= peso / altura ** 2
 print( "Su índice de masa corporal es: ",imc)
+
+#punto 10
+celsius = float(input("Ingrese la termpera Celsius: "))
+fahrenheit= celsius * 9/5 + 32
+print(f"La trasformación de grados celsius a Fahrenheit es: {fahrenheit} ")
+
+#punt11
+hora_trabajada = int(input("Ingrese el número de horas trabajadas: "))
+costo_por_hora = float(input("Ingrese el costo por hora: "))
+print("El sueldo que le corresponde es", hora_trabajada*costo_por_hora)
+
+#punto 12
