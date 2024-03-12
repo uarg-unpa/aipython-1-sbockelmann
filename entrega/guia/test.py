0ce391253a41aed5905d94918b4cdaa7573ca91c
sbockelmann <161514307+sbockelmann@users.noreply.github.com>
2024-03-12 19:31:14 +0000

    cambio

---
+++ entrega/guia/test.py
@@ -0,0 +1,10 @@
+print("Nombre\tEdad\tPais\tCiudad")
+print("Alexa\t250\tUSA\tCapeCod")
+
+edad_usuario1=int (input("Ingrese la edad del cliente: "))
+if edad_usuario1 < 4:
+    print("puede pasar gratis")
+elif edad_usuario1 > 4 and edad_usuario1 < 8:
+    print("Debe pagar $5")
+else:
+    print("Debe pagar $10"
