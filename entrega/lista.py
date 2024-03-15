58557a24fea6b4fd9098a8c5351a189cf0324fbc
sbockelmann <161514307+sbockelmann@users.noreply.github.com>
2024-03-15 18:33:13 +0000

    cambio02

---
+++ entrega/lista.py
@@ -0,0 +1,14 @@
+#lista
+#crear una lista
+#nombnre=[]
+#valor inicial
+nombre=['franco', 'fernanda', 'Alejandro','Fabiana']
+#Nombre lista
+#print (nombre)
+#interar sobre la lista usando indice
+for i in range(len(nombre)):
+    print(nombre[i])
+#acceder a los elementos
+#acceder al primer elemento
+primer_elemento=nombre[0]
+print(f"El primer elemento es {primer_elemento}")
